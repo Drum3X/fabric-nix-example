0a0f6a6a83b8deaf1eda2c84578e7283b6cee705
wholikeel <mail@michaellepera.xyz>
2024-09-04 19:24:37 +0930

    change derivation

--- bar-example/derivation.nix
+++ bar-example/derivation.nix
@@ -1,4 +1,4 @@
-{ lib, python3Packages, gtk3, gtk-layer-shell, cairo, gobject-introspection, libdbusmenu-gtk3, gdk-pixbuf, gnome, cinnamon, wrapGAppsHook3}:
+{ lib, python3Packages, gtk3, gtk-layer-shell, cairo, gobject-introspection, libdbusmenu-gtk3, gdk-pixbuf, gnome, cinnamon, wrapGAppsHook3, ...}:
 
 python3Packages.buildPythonApplication {
   pname = "fabric-nix-example";
@@ -13,13 +13,13 @@ python3Packages.buildPythonApplication {
     gobject-introspection
     cairo
   ];
-  buildInputs = [
-    libdbusmenu-gtk3
-    gtk-layer-shell
-    gnome.gnome-bluetooth
-    cinnamon.cinnamon-desktop
-    gdk-pixbuf
-  ];
+  # buildInputs = [
+  #   libdbusmenu-gtk3
+  #   gtk-layer-shell
+  #   gnome.gnome-bluetooth
+  #   cinnamon.cinnamon-desktop
+  #   gdk-pixbuf
+  # ];
 
   dependencies = with python3Packages; [ python-fabric ];
   doCheck = false;
